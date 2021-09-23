874dbca622de7bd0ed579512e2e2d0049faed99f
kungc <kungcheow@gmail.com>
2021-09-22 17:22:21 -0700

    Fix static not load in heroku

--- backend/server.js
+++ backend/server.js
@@ -28,9 +28,10 @@ mongoose.connection.once('open', () => {
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static('../frontend/build'));
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '..', 'frontend' ,'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
     });
 }
 
