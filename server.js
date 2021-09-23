f88517854a16b920a8b55fec6e701df6f67ab172
kungc <kungcheow@gmail.com>
2021-09-22 23:52:48 -0700

    Fix dirname undefined issue

--- server.js
+++ server.js
@@ -26,9 +26,9 @@ mongoose.connection.once('open', () => {
 
 const __dirname = path.resolve();
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, '/frontend/build')));
+    app.use(express.static('frontend/build'));
     app.get('*', (req, res) =>
-        res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+        res.sendFile(path.join(__dirname, 'frontend/build/index.html'))
     );
 }
 
