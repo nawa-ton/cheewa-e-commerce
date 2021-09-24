bc82917078d0bac68a13055cfa57dc23862520e0
kungc <kungcheow@gmail.com>
2021-09-23 19:48:40 -0700

    update backend url for local test

--- frontend/src/constants/urlConstants.js
+++ frontend/src/constants/urlConstants.js
@@ -1,2 +1,2 @@
-export const backendUrl = "https://cheewa-ecommerce.herokuapp.com";
-//export const backendUrl = "";
+//export const backendUrl = "https://cheewa-ecommerce.herokuapp.com";
+export const backendUrl = "";
